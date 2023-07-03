9a657e82ebf405841b018ed798c3848895324fed
João Pedro Lanzilotti Ferretti <71307203+joaosinho2@users.noreply.github.com>
2023-07-03 14:56:38 -0300

    API Initialized

---
+++ rest-api/api.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const api = express();
+const port = 3000;
+const router = express.Router();
+
+api.use(cors());
+
+api.use(bodyParser.urlencoded({extended: true}));
+api.use(bodyParser.json({limit: '20mb', extended: true}));
+
+router.get("/", (request, response) =>  response.json({
+    mensagem: 'API On...'
+}));
+
+api.use("/", router);
+
+api.listen(port);
+console.log("Run API Express is On");
